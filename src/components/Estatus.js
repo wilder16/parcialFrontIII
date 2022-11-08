e98098f6f446a8afadef5a339846659afb33d492
ctd-frontend <92804884+ctd-frontend@users.noreply.github.com>
2022-11-08 14:07:29 -0300

    Update Estatus.js

--- src/components/Estatus.js
+++ src/components/Estatus.js
@@ -3,7 +3,7 @@ import React from 'react'
 // El componente Estatus no tiene componentes hijos.
 // ESTADO: Estatus no tiene estado.
 // MÉTODOS: Estatus no tiene métodos.
-// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente.
+// PROPS: Estatus recibe de su padre la cantidad que va a mostrar dentro del span correpondiente.
 // Maqueta de Estatus:
 //    h1
 //    p > span     (el span mostrará la cantidad recibida por props)
